36911182fecf44f9190099c14cbbc4c2c3ea70f0
David Nguyen <david.nguyen@terralogic.com>
2022-06-06 10:49:16 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by David Nguyen on 06/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
